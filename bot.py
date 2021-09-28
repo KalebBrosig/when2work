510b70937ca06a0d8321aeb4eb8559e658a11f1d
Kaleb Brosig <kalebbrosig@icloud.com>
2021-09-27 21:26:43 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,93 @@
+import sqlite3
+import discord
+import os
+import w2w
+
+def getEIDByDiscordID(DiscordID: str) -> str: # EID
+    try:
+        con = sqlite3.connect("w2w.db")
+        cur = con.cursor()
+
+        cur.execute('SELECT "EID" FROM `Employees` WHERE "DiscordID" IS ?;', (DiscordID,))
+        return str(cur.fetchone()[0])
+    except:
+        return None
+    finally:
+        con.close()
+
+def getPositionsByEID(EID: str) -> list[str]: # [SkillID]
+    try:
+        con = sqlite3.connect("w2w.db")
+        cur = con.cursor()
+
+        cur.execute('SELECT "Skills" FROM `Employees` WHERE "EID" IS ?;', (EID,))
+        return [str(idx) for idx in cur.fetchone()[0].split(",")[0:-1]]
+    except:
+        return None
+    finally:
+        con.close()
+
+RoleIDs = { # {SkillID: RoleID,}
+    310853183: 891922814423142480, # Arcade
+    310853184: 892074545379409930, # Control Counter
+    415325361: 891922939614756925, # Go-Carts
+    324621987: 892074694386266172, # Kitchen Team
+    312463108: 892074840608100373, ##Maintenance
+    322487716: 892074964096811009, ##Manager
+    315805767: 892075133244694569, # Office
+    483317720: 892075228736413736, # P.T. Sports
+    310853187: 892075334051172362, # Snack Bar
+    426599712: 892075427303137291, ##Team Lead
+    316580542: 891922967162929162, # Training
+    486535897: 892075550011719710, # Bowling League Attendant
+    310853186: 892075758116302849, # Party Host/Hostess
+    538416832: 892075958268481626, # Escape Room
+    310853506: 892076050174066718, # Float
+    656121611: 892076183636807740, # Axe Throwing
+    677654691: 892074545379409930, # Control Counter
+    486535895: 892077178508967936, # PT Island
+    656121608: 892077227590701127, # PT Social
+    415325359: 892077274491420713, # Grounds Team
+    604175097: 892077319563382784, # Supervised Play
+    415327590: 892077387905380392, # Banquet
+    400090436: 892077425045930054, # Janitorial
+    538411160: 892077465990725662, # The Lab
+    489143721: 892078326770991124, # Manager Meeting
+}
+
+# Discord
+
+TOKEN, GUILD = os.getenv("w2wD"), int(os.getenv("w2wG"))
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"{client.user} Ready")
+
+    guild: discord.guild.Guild = client.get_guild(GUILD)
+    for member in guild.members:
+        eid = getEIDByDiscordID(str(member.id))
+        if eid == None:
+            print(member.name, "Not found in database!")
+            continue
+        if w2w.isDeleted(eid) == "True": # Not of bool type
+            await member.kick() # when testing do not include this line!!!!!!!
+            print(f"Kicked {member.name}")
+            continue
+        
+        print(member.name)
+        for SkillID in getPositionsByEID(eid):
+            print("    ", SkillID)
+            RoleID = int(RoleIDs[int(SkillID)])
+            try:
+                await member.add_roles(guild.get_role(RoleID)) # when testing do not include this line!!!!!!!!!!!
+            except discord.errors.Forbidden:
+                print("Tried to assign higher than current rank!")
+                pass
+
+    await client.close()
+
+client.run(TOKEN)
---
+++ scrape.py
@@ -0,0 +1,153 @@
+import os
+import time
+import json
+import atexit
+import sqlite3
+import urllib.parse
+from typing import List
+from dateutil import parser
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class shift:
+    def __init__(self, EID, EName, Position, Description, Start, End, SkillID, ShiftID):
+        self.EID = EID
+        self.EName = EName if EName.find("(deleted)") == -1 else EName.replace(" (deleted)", "")
+        self.Position = Position
+        self.Description = Description
+        self.Date = datetime.fromtimestamp(Start).date()
+        self.Start = Start
+        self.End = End
+        self.Duration = (End - Start) if End > Start else ((End + 86400) - Start) # Correct for midnight and later shifts
+        self.Deleted = str(EName.find("(deleted)") != -1)
+        self.SkillID = SkillID
+        self.ShiftID = ShiftID
+
+class employee:
+    def __init__(self, EID, EName, Deleted):
+        self.EID = EID
+        self.EName = EName
+        self.Deleted = Deleted
+        self.Skills = str()
+
+    def setRank(self):
+        if self.Skills is None: return
+        if (self.Skills.find("322487716") != -1):
+            self.Rank = "Manager"
+        elif (self.Skills.find("426599712") != -1):
+            self.Rank = "Team Lead"
+        elif (self.Skills.find("312463108") != -1):
+            self.Rank = "Maintenance"
+        else:
+            self.Rank = "Employee"
+
+def setup() -> webdriver:
+    driver: webdriver = webdriver.Safari()
+    atexit.register(driver.close)
+
+    driver.get("https://whentowork.com/login.htm")
+    WebDriverWait(driver, 10).until(EC.title_contains("W2W Sign In"))
+    time.sleep(1) # Load JS
+    username, password = str(os.getenv("w2wU")), str(os.getenv("w2wP"))
+    driver.find_element_by_id("username").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    while True: 
+        if driver.find_element_by_id("password").get_attribute("value") == password: break
+    driver.find_element_by_id("password").send_keys(Keys.ENTER)
+    WebDriverWait(driver, 10).until(EC.title_contains("Home"))
+
+    return driver
+
+def scrapeShifts(driver: webdriver, start: int = int(time.time()), end: int = 1259388000):
+    con = sqlite3.connect("w2w.db")
+    cur = con.cursor()
+
+    currentTime = start
+    while currentTime > end:
+        Date = datetime.fromtimestamp(currentTime).strftime("%m/%d/%Y")
+        print(Date) # to show progress
+
+        driver.execute_script(f"ReplWin('empfullschedule','&View=Day&Date='+'{Date}');")
+        WebDriverWait(driver, 10).until(EC.title_contains("Schedule"), EC.url_contains("empfullschedule"))
+        time.sleep(0.75) # Load Elements
+        assert Date in driver.current_url
+
+        POS, SID = "", ""
+        for item in driver.find_elements_by_xpath('//*[@id="maincontent"]/table[1]/tbody/tr[2]/td/table/tbody/script'):
+            func = urllib.parse.unquote(item.text)
+            if func[0:3] == "php":
+                func = func.split('"')
+                POS, SID = func[5], func[1]
+                continue
+            func = func.split(";")
+            hdr = func[0].split("\"")
+            cl = func[[idx for idx, element in enumerate(func) if element.__contains__("cl(")][0]].split(",")
+            clidx = [idx for idx, s in enumerate(cl) if '{' in s][0]
+            ShiftJson = json.loads(", ".join(cl[clidx:])[1:-2])
+            assert SID == ShiftJson["skillid"]
+
+            EID, EName = hdr[2].split(",")[2], hdr[3]
+            ShiftID = cl[3][1:-1]
+            startTime = int(parser.parse(f"{Date} {ShiftJson['start']}").timestamp())
+            endTime = int(parser.parse(f"{Date} {ShiftJson['end']}").timestamp())
+
+            Shift = shift(EID, EName, POS, ShiftJson["description"], startTime, endTime, SID, ShiftID)
+            
+            cur.execute('SELECT * FROM `Shifts` WHERE "ShiftID" IS ?;', (Shift.ShiftID,))
+            if len(cur.fetchall()) == 0:
+                cur.execute('INSERT INTO `Shifts` ("EID", "EName", "Position", "Description", "Date", "Start", "End", "Duration", "Deleted", "SkillID", "ShiftID") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', (Shift.EID, Shift.EName, Shift.Position, Shift.Description, Shift.Date, Shift.Start, Shift.End, Shift.Duration, Shift.Deleted, Shift.SkillID, Shift.ShiftID,))
+            else:
+                cur.execute('UPDATE `Shifts` SET "EID" = ?, "EName" = ?, "Deleted" = ? WHERE "ShiftID" IS ?', (Shift.EID, Shift.EName, Shift.Deleted, Shift.ShiftID,))
+
+            cur.execute('SELECT * FROM `Employees` WHERE "EID" IS ?;', (Shift.EID,))
+            if len(cur.fetchall()) == 0:
+                cur.execute('INSERT INTO `Employees` ("EID", "EName", "Deleted") VALUES (?, ?, ?);', (Shift.EID, Shift.EName, Shift.Deleted,))
+            else:
+                cur.execute('UPDATE `Employees` SET "Deleted" = ? WHERE EID IS ?;', (Shift.Deleted, Shift.EID,))
+
+        con.commit()
+        currentTime -= 86400
+    con.commit()
+    con.close()
+
+def scrapeEmployees(driver: webdriver):
+    eids = dict() # {EID: {POSID, POSID}}
+    con = sqlite3.connect("w2w.db")
+    cur = con.cursor()
+
+    driver.execute_script(f"ReplWin('empemplist.htm','')")
+    WebDriverWait(driver, 10).until (EC.title_contains("Staff List"))
+    assert "empemplist" in driver.current_url
+    time.sleep(1)
+        
+    for SkillID in [idx.get_attribute("value") for idx in driver.find_elements_by_xpath("/html/body/div[2]/table/tbody/tr/td[2]/select/option")]:
+        if SkillID == '' or int(SkillID) < 0: continue
+        print(SkillID)
+        Select(driver.find_element_by_xpath("/html/body/div[2]/table/tbody/tr/td[2]/select")).select_by_value(SkillID)
+        time.sleep(1) # implicit wait to change the value (change to dynamic later)
+        for item in [idx for idx in driver.find_elements_by_xpath('//*[@id="maincontent"]/table[1]/tbody/tr[2]/td/table/tbody/tr')]:
+            try:
+                EID = item.find_elements_by_xpath('./td[1]/*')[0].get_attribute("onclick").split("'")[1]
+                print(EID)
+                if EID not in eids: eids[EID] = set()
+                eids[EID].add(SkillID)
+            except:
+                pass
+
+    for EID, Skills in eids.items():
+        cur.execute('SELECT * FROM `Employees` WHERE "EID" IS ?;', (EID,))
+        emp = cur.fetchone()
+        try:
+            empobj = employee(emp[0], emp[1], emp[2])
+            for SkillID in Skills:
+                empobj.Skills = empobj.Skills + SkillID + ","
+            empobj.setRank()
+        except:
+            continue # temp fix for random EIDS
+        cur.execute('UPDATE `Employees` SET "Skills" = ?, "Rank" = ? WHERE EID IS ?;', (empobj.Skills, empobj.Rank, EID,))
+    con.commit()
+    con.close()
